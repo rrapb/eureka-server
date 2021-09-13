4e412e9ab1ace8b1b7146099a9d575a276abd31f
Rrap Braina <rb46757@ubt-uni.net>
2021-09-13 19:15:41 +0100

    * initial commit

---
+++ src/test/java/com/ubt/gymmanagementsystem/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ubt.gymmanagementsystem.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
